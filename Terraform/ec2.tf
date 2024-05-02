29ff42e395fc3527f1166f3e4f6cd03f219983e2
Kenny Saenz <saenzk031@gmail.com>
2024-05-01 21:47:53 -0600

    Upload Ansible Files & Add keypairs on Terraform files

--- Terraform/ec2.tf
+++ Terraform/ec2.tf
@@ -2,7 +2,8 @@ resource "aws_instance" "web" {
   ami           = "ami-04b70fa74e45c3917"
   instance_type = "t2.micro"
   count = 2
-
+  key_name = "tf_key"
+  
   tags = {
     Name = "dockerapp"
   }
